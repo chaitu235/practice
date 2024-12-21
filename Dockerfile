a1fe83c3d47b1501c0d8654e9d99f8e95a1a3b48
Chaitanya Patil <chetup988@gmail.com>
2024-12-21 19:00:50 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM openjdk:11
+
+WORKDIR /app
+
+COPY  . /app
+
+RUN javac Test.java
+
+CMD [ "java","Test" ]
+
+
+
+
+
+
+
---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        // Print Hello World to the console
+        System.out.println("Hello World!!!");
+    }
+}
